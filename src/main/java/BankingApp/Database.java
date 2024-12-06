b975911dc8bd0f43fa7d44675d3a80070a32d5aa
Kerby <rubinas.kerby@gmail.com>
2024-12-06 20:54:19 +0800

    added option to add bank account to existing user and option to truncate tables.

--- src/main/java/BankingApp/Database.java
+++ src/main/java/BankingApp/Database.java
@@ -6,95 +6,30 @@ import java.sql.*;
 
 public class Database {
     String databasePath;
+    private static Server consoleServer;
 
-    public Database(String databasePath) throws SQLException {
+    public Database(String databasePath)  {
         this.databasePath = databasePath;
         generateDatabase(databasePath);
     }
-
-    public void add(Customer customer, Double initialDeposit) throws SQLException {
-        try (Connection connection = createConnectionAndEnsureDatabase(databasePath)) {
-            // Check for uniqueness of username
-            String userQuery = "SELECT COUNT(*) FROM users WHERE username = ?";
-            try (PreparedStatement userStmt = connection.prepareStatement(userQuery)) {
-                userStmt.setString(1, customer.getUserName());
-                try (ResultSet result = userStmt.executeQuery()) {
-                    if (result.next() && result.getInt(1) > 0) {
-                        System.out.println("Username '" + customer.getUserName() + "' already exists. Skipping insert.");
-                        return; // Early exit for duplicates
-                    }
-                }
-            }
-
-            String insertQuery = "INSERT INTO users (first_name, last_name, username, password) VALUES (?, ?, ?, ?)";
-            try (PreparedStatement insertStmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS)) {
-                insertStmt.setString(1, customer.getFirstName());
-                insertStmt.setString(2, customer.getLastName());
-                insertStmt.setString(3, customer.getUserName());
-                insertStmt.setString(4, customer.getPassword());
-                insertStmt.executeUpdate();
-                System.out.println("User '" + customer.getUserName() + "' added successfully.");
-
-                try (ResultSet generatedKeys = insertStmt.getGeneratedKeys()) {
-                    if (generatedKeys.next()) {
-                        int userId = generatedKeys.getInt(1);
-                        String insertAccountQuery = "INSERT INTO accounts (account_number, user_id, fund_amount, loan_amount) VALUES (?, ?, ?, ?)";
-                        try (PreparedStatement accountStmt = connection.prepareStatement(insertAccountQuery)) {
-                            accountStmt.setString(1, customer.getAccountNumber());
-                            accountStmt.setInt(2, userId);
-                            accountStmt.setDouble(3, initialDeposit);
-                            accountStmt.setDouble(4, 0.0);
-                            accountStmt.executeUpdate();
-                            System.out.println("Customer and account created successfully.");
-                        }
-                    } else {
-                        System.out.println("Failed to retrieve user ID. Account creation aborted.");
-                    }
-                }
-            }
-        }
-    }
-
-    public void createAccount(String username, String password) throws SQLException {
-        try (Connection connection = createConnectionAndEnsureDatabase(databasePath)) {
-            String userQuery = "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?";
-            try (PreparedStatement accountStmt = connection.prepareStatement(userQuery)) {
-                accountStmt.setString(1, username);
-                accountStmt.setString(2, password);
-                try (ResultSet result = accountStmt.executeQuery()) {
-                    if (result.next() && result.getInt(1) == 0) {
-                        System.out.println("User not found. Retry input.");
-                        return;
-                    }
-                }
-                System.out.println("Account found. Generating account number...");
-            }
-        }
-    }
-
 
     private Connection createConnectionAndEnsureDatabase(String databasePath) throws SQLException {
         String user = "admin";
         String password = "1234";
 
-        Connection connection = DriverManager.getConnection(databasePath, user, password);
-
-        return connection;
+        return DriverManager.getConnection(databasePath, user, password);
     }
 
     /**
      * Generate H2 Database to store credentials and fund amount of customers
-     * @throws SQLException
      */
-    public static void generateDatabase(String databasePath) throws SQLException {
+    public static void generateDatabase(String databasePath) {
         String user = "admin";
         String password = "1234";
 
-        Server console = null;
-
         try {
             // Start H2 Console Server
-            console = Server.createTcpServer("-tcpAllowOthers", "-tcpPort", "9092").start();
+            consoleServer = Server.createTcpServer("-tcpAllowOthers", "-tcpPort", "9092").start();
             Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
             System.out.println("H2 Console started at: http://localhost:8082");
 
@@ -136,4 +71,141 @@ public class Database {
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+    public void stopDatabase() {
+        if (consoleServer != null) {
+            try {
+                if (consoleServer.isRunning(false)) {
+                    consoleServer.stop();
+                    System.out.println("H2 Console stopped.");
+                } else {
+                    System.out.println("H2 Console is not running.");
+                }
+            } catch (Exception e) {
+                System.err.println("Error while stopping H2 Console: " + e.getMessage());
+            }
+        } else {
+            System.out.println("H2 Console server instance is null. Nothing to stop.");
+        }
+    }
+
+    public boolean checkAccountExistence(String username, String password) throws SQLException {
+        try (Connection connection = createConnectionAndEnsureDatabase(databasePath)) {
+            String userQuery = "Select COUNT(*) FROM users WHERE username = ? AND password = ?";
+
+            try (PreparedStatement accountStmt = connection.prepareStatement(userQuery)) {
+                accountStmt.setString(1, username);
+                accountStmt.setString(2, password);
+
+                try (ResultSet result = accountStmt.executeQuery()) {
+                    if (result.next() && result.getInt(1) == 0) {
+                        System.out.println("User not found. Retry input.");
+                        return false;
+                    }
+                }
+                System.out.println("Account found. Generating new account number...");
+                return true;
+            }
+        }
+    }
+
+    public void add(Customer customer, Double initialDeposit) throws SQLException {
+        try (Connection connection = createConnectionAndEnsureDatabase(databasePath)) {
+            // Check for uniqueness of username
+            String userQuery = "SELECT COUNT(*) FROM users WHERE username = ?";
+            try (PreparedStatement userStmt = connection.prepareStatement(userQuery)) {
+                userStmt.setString(1, customer.getUserName());
+                try (ResultSet result = userStmt.executeQuery()) {
+                    if (result.next() && result.getInt(1) > 0) {
+                        System.out.println("Username '" + customer.getUserName() + "' already exists. Skipping insert.");
+                        return; // Early exit for duplicates
+                    }
+                }
+            }
+
+            String insertQuery = "INSERT INTO users (first_name, last_name, username, password) VALUES (?, ?, ?, ?)";
+            try (PreparedStatement insertStmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS)) {
+                insertStmt.setString(1, customer.getFirstName());
+                insertStmt.setString(2, customer.getLastName());
+                insertStmt.setString(3, customer.getUserName());
+                insertStmt.setString(4, customer.getPassword());
+                insertStmt.executeUpdate();
+
+                try (ResultSet generatedKeys = insertStmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int userId = generatedKeys.getInt(1);
+                        String insertAccountQuery = "INSERT INTO accounts (account_number, user_id, fund_amount, loan_amount) VALUES (?, ?, ?, ?)";
+                        try (PreparedStatement accountStmt = connection.prepareStatement(insertAccountQuery)) {
+                            accountStmt.setString(1, customer.getAccountNumber());
+                            accountStmt.setInt(2, userId);
+                            accountStmt.setDouble(3, initialDeposit);
+                            accountStmt.setDouble(4, 0.0);
+                            accountStmt.executeUpdate();
+                            System.out.println("Customer and account created successfully.");
+                        }
+                    } else {
+                        System.out.println("Failed to retrieve user ID. Account creation aborted.");
+                    }
+                }
+            }
+        }
+    }
+
+    public void generateBankAccount(String username, Double initialDeposit) throws SQLException {
+        try (Connection connection = createConnectionAndEnsureDatabase(databasePath)) {
+            String userQuery = "SELECT id FROM users WHERE username = ?";
+            try (PreparedStatement accountStmt = connection.prepareStatement(userQuery)) {
+                accountStmt.setString(1, username);
+                try (ResultSet result = accountStmt.executeQuery()) {
+                    if (result.next()) {
+                        int userId = result.getInt("id");
+                        Account newBankAccount = new Account();
+                        String insertAccountQuery = "INSERT INTO accounts (account_number, user_id, fund_amount, loan_amount) VALUES (?, ?, ?, ?)";
+                        try (PreparedStatement insertAccountStmt = connection.prepareStatement(insertAccountQuery)) {
+                            insertAccountStmt.setString(1, newBankAccount.getBankAccountNumber());
+                            insertAccountStmt.setInt(2, userId);
+                            insertAccountStmt.setDouble(3, initialDeposit);
+                            insertAccountStmt.setDouble(4, 0.0);
+                            insertAccountStmt.executeUpdate();
+                            System.out.println("Bank account for " + username + " created successfully.");
+                        }
+                    } else {
+                        System.out.println("User with username '" + username + "' does not exist.");
+                    }
+                }
+            }
+        }
+    }
+
+    public void hardReset() throws SQLException {
+        String userQuery1 = "TRUNCATE TABLE users";
+        String userQuery2 = "TRUNCATE TABLE accounts";
+        String resetUserIdSequence = "ALTER TABLE users ALTER COLUMN id RESTART WITH 1";
+        String resetAccountIdSequence = "ALTER TABLE accounts ALTER COLUMN id RESTART WITH 1";
+
+        try (Connection connection = createConnectionAndEnsureDatabase(databasePath)) {
+            Statement stmt = connection.createStatement();
+
+            // Start a transaction
+            connection.setAutoCommit(false);
+
+            try {
+                stmt.executeUpdate("SET REFERENTIAL_INTEGRITY FALSE");
+                stmt.executeUpdate(userQuery1);
+                stmt.executeUpdate(userQuery2);
+                stmt.executeUpdate(resetUserIdSequence);
+                stmt.executeUpdate(resetAccountIdSequence);
+                stmt.executeUpdate("SET REFERENTIAL_INTEGRITY TRUE");
+
+                connection.commit();
+                System.out.println("Tables 'users' and 'accounts' truncated successfully");
+
+            } catch (Exception e) {
+                connection.rollback();
+                System.out.println("Error in truncating tables, transaction rolled back");
+                throw e;
+            }
+        }
+    }
+
 }
--- src/main/java/BankingApp/TextUIApplication.java
+++ src/main/java/BankingApp/TextUIApplication.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class TextUIApplication {
-    private Scanner scanner;
+    private final Scanner scanner;
     private final Database database;
 
     public TextUIApplication(Scanner scanner, Database database) {
@@ -16,10 +16,12 @@ public class TextUIApplication {
         System.out.println("Hello! Welcome to my banking app!");
 
         while (true) {
+            printTextInstructions();
             System.out.print("Please select operation: ");
             int  input = Integer.parseInt(scanner.nextLine());
 
             if (input == 0) {
+                this.database.stopDatabase();
                 System.out.println("Thanks. Have a good day!");
                 break;
             }
@@ -29,8 +31,15 @@ public class TextUIApplication {
             if (input == 2) {
                 CreateBankAccount();
             }
+            if (input == 9) {
+                adminControl();
+            }
         }
 
+    }
+
+    private void adminControl() throws SQLException {
+        this.database.hardReset();
     }
 
     private void CreateCustomerAccount() throws SQLException {
@@ -44,23 +53,44 @@ public class TextUIApplication {
         System.out.print("Password: ");
         String password = scanner.nextLine();
 
-        System.out.print("Initial deposit: ");
-        Double initialDeposit = Double.valueOf(scanner.nextLine());
-
+        Double initialDeposit = readDeposit();
+        if (initialDeposit == null) {
+            return;
+        }
         Customer newCustomer = new Customer(firstName, lastName, username, password);
-
-
         this.database.add(newCustomer, initialDeposit);
     }
-
 
     private void CreateBankAccount() throws SQLException {
         System.out.print("Input username: ");
         String username = scanner.nextLine();
         System.out.print("Input password: ");
         String password = scanner.nextLine();
-        this.database.createAccount(username, password);
 
+        if (this.database.checkAccountExistence(username, password)) {
+            Double initialDeposit = readDeposit();
+            if (initialDeposit == null) {
+                return;
+            }
+            this.database.generateBankAccount(username, initialDeposit);
+        }
+    }
+
+    private Double readDeposit() {
+        Double deposit = null;
+        try {
+            System.out.print("Initial deposit: ");
+            deposit = Double.valueOf(scanner.nextLine());
+
+            if (deposit <= 0.0) {
+                System.out.println("Invalid value. Deposit must be greater than 0.");
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid number for the deposit.");
+        }
+        return deposit;
     }
 
     private void printTextInstructions() {
@@ -72,5 +102,4 @@ public class TextUIApplication {
         System.out.println("5: Apply for Loan");
         System.out.println("6: Pay Loan");
     }
-
 }
